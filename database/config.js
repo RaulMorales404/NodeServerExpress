a72d7ccc281e0acd3002fe83ba7398949daa9c38
RaulMorales404 <r20160106@gmail.com>
2024-10-16 21:44:07 -0600

    addd parameters de conexion

--- database/config.js
+++ database/config.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const conectionDB = async () => {
   try {
     const url = process.env.MONGO_DB_ATLAS;
-    await mongoose.connect('mongodb+srv://user_cofe:VbJqAas3HJ4q7GiH@cluster0.yihfw.mongodb.net/cafeDB');
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Asegura el uso de la topología unificada
+    });
     console.log("Conexicon Online");
   } catch (error) {
     console.log("Error:", error);
