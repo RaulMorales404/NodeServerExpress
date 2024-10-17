5857f9fc7a6a04afd143308268d042880942cf11
RaulMorales404 <r20160106@gmail.com>
2024-10-17 02:35:51 -0600

    change parameters conectiondb

--- database/config.js
+++ database/config.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const conectionDB = async () => {
   try {
     const url = process.env.MONGO_DB_ATLAS;
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, // Asegura el uso de la topología unificada
-    });
+    await mongoose.connect(url);
     console.log("Conexicon Online");
   } catch (error) {
     console.log("Error:", error);
