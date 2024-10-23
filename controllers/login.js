880d5c59d70723d016d8118f8d11c1028debe9da
RaulMorales404 <r20160106@gmail.com>
2024-10-23 01:00:12 -0600

    create user width google acount

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,8 @@
 const { response, request } = require("express");
 const bcriptjs = require("bcryptjs");
 const userModel = require("./../models/usuarios");
-const  generateJWT = require('../helpers/jwtGenerate.js');
+const generateJWT = require("../helpers/jwtGenerate.js");
+const { GoogleVerify } = require("./../helpers/googleVerify");
 
 const postLogin = async (req = request, res = response) => {
   try {
@@ -19,15 +20,14 @@ const postLogin = async (req = request, res = response) => {
         msj: "email/password incorrectos- Password",
       });
     }
- 
+
     const token = await generateJWT(user.id);
-    
 
     res.json({
-      email, 
+      email,
       password,
       user,
-      token
+      token,
     });
   } catch (error) {
     console.log("Error::", error);
@@ -37,6 +37,48 @@ const postLogin = async (req = request, res = response) => {
   }
 };
 
+const postLoginGoogle = async (req = request, res = response) => {
+  const id_token = req.body;
+  try {
+    const { name, img, email } = await GoogleVerify(id_token);
+  
+    let user = await userModel.findOne({ email });
+ 
+    if (!user) {
+      const data = {
+        name,
+        email,
+        password: ":P",
+        img,
+        rol:'USER_ROL',
+        googleAcount: true,
+      };
+      user = new userModel(data);
+      await user.save();
+    }
+  
+
+    if (!user.state) {
+      return res.status(401).json({
+        msj: "Hable con el administrador Usuario bloqueado",
+      });
+    }
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log("Error::", error);
+    res.status(400).json({
+      ok: false,
+      msj: "No se completo la authenticacion Token invalido",
+    });
+  }
+};
+
 module.exports = {
   postLogin,
+  postLoginGoogle,
 };
